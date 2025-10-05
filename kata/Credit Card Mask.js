c74ca0c5ecd5603150b5d982cc67597a0068485c
Johnny9874 <ye.johnnypro@gmail.com>
2025-10-05 13:41:06 +0200

    Ajout dossier kata

---
+++ kata/Credit Card Mask.js
@@ -0,0 +1,30 @@
+/*
+Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+Examples (input --> output):
+"4556364607935616" --> "############5616"
+     "64607935616" -->      "#######5616"
+               "1" -->                "1"
+                "" -->                 ""
+
+// "What was the name of your first pet?"
+"Skippy" --> "##ippy"
+"Nananananananananananananananana Batman!" --> "####################################man!"
+*/
+
+
+// Solution apporté :
+/*
+  We need to mask off all caracter but the last four characters into #
+  
+  Quand user rentre string dans maskify:
+    - Convertir chaque caractère en #
+    - Si on arrive a longueur de la chaine - 4 s'arreter
+*/
+function maskify(cc) {
+    cc = cc.split('');
+    for (let i = 0; i < cc.length - 4; i++) cc[i] = '#';
+    return cc.join("");
+  }
---
+++ kata/README.md
@@ -0,0 +1,3 @@
+# Listing de tout les kata réalisé sur codewars.
+
+Dans ce dossier vous trouverez différent fichier javascript traitant un problème proposer par le site codewars ainsi que l'énoncé.
