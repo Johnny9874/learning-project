bca7298f48600019ed55232788a3659816724e21
Johnny9874 <ye.johnnypro@gmail.com>
2025-10-07 00:00:49 +0200

    Début 30 days challenge python 1er jour validé

---
+++ 30DaysOfPython/day_1/helloworld.py
@@ -0,0 +1,43 @@
+from math import dist
+
+print("2eme exo : ")
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+print("Johnny")
+print("Ye")
+print("France")
+print("I am enjoying 30 days of python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Johnny"))
+print(type("Ye"))
+print(type("France"))
+
+print("3eme exo: ")
+
+print(type(3))
+print(type(3.5))
+print(type(4+5j))
+print(type("ALex"))
+print(type(True))
+print(type(['Jean', 'Alex','Angela']))
+print(type(('This', 'is', 'cool')))
+print(type({
+    'name' : 'alex',
+    'age' : 18,
+    'height' : 182
+}))
+print(type({'This', 'is', 'dope'}))
+
+print(dist((2, 3), (10, 8)))
